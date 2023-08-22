82a50715b38ddbef4198f821aa4d0357d9c7d2a6
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-22 22:41:57 +0200

    add loading skeleton component

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,17 +1,26 @@
 import { useEffect } from "react";
-import Button from "./Button";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { fetchUsers } from "../store";
+import Skeleton from "./Skeleton";
 
 export default function Users() {
 
     const dispatch = useDispatch();
 
+    const { data, loading, error } = useSelector(state => state.users);
+
     useEffect(() => {
         dispatch(fetchUsers());
     }, []);
 
+    if (loading) {
+        return <Skeleton times={6} className={"h-10 w-full"} />
+    }
+    if (error) {
+        return <div>Error during data fetching...</div>
+    }
+
     return <div>
-        <Button variation={"primary"}>Prova</Button>
+        {data.length}
     </div>
 }
