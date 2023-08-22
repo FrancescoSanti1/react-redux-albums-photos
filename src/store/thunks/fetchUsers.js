badf96972603c5e34339408e07952136822f5145
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-22 22:41:36 +0200

    add sleep to test loading skeleton component

--- src/store/thunks/fetchUsers.js
+++ src/store/thunks/fetchUsers.js
@@ -3,7 +3,13 @@ import axios from "axios";
 
 const fetchUsers = createAsyncThunk("users/fetch", async () => {
     const response = await axios.get("http://localhost:3005/users");
+
+    await sleep(2);
     return response.data;
 });
+
+const sleep = (seconds) => new Promise(resolve => {
+    setTimeout(resolve, seconds * 1000);
+})
 
 export { fetchUsers };
