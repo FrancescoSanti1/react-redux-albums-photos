21ce2e0c4fa3930618c9bcafebc3b5e3443c35ba
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-08-31 22:37:00 +0200

    fix double import

--- src/store/index.js
+++ src/store/index.js
@@ -2,7 +2,6 @@ import { configureStore } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/users";
 import { fetchUsers } from "./thunks/fetchUsers";
 import { addUser } from "./thunks/addUser";
-import { addUser } from "./thunks/addUser";
 import { deleteUser } from "./thunks/deleteUser";
 
 const store = configureStore({
