5c15968217b2a5840ad8e6d081168467f667b85d
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-09-01 22:09:14 +0200

    add store config for RTK query

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,20 @@
-import { configureStore } from "@reduxjs/toolkit";
+import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import { usersReducer } from "./slices/users";
 import { fetchUsers } from "./thunks/fetchUsers";
 import { addUser } from "./thunks/addUser";
 import { deleteUser } from "./thunks/deleteUser";
+import { albumsApi } from "./apis/albums";
+import { setupListeners } from "@reduxjs/toolkit/dist/query";
 
 const store = configureStore({
     reducer: {
-        users: usersReducer
-    }
+        users: usersReducer,
+        [albumsApi.reducerPath]: albumsApi.reducer
+    },
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(albumsApi.middleware)
 });
 
+setupListeners(store.dispatch);
+
 export { store, fetchUsers, addUser, deleteUser };
+export { useGetAlbumsQuery } from "./apis/albums";
