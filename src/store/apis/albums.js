45a63bb382feb4094afdd3a595c4b72ac365c6cd
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-09-02 11:51:13 +0200

    add new endpoint for adding an album

--- src/store/apis/albums.js
+++ src/store/apis/albums.js
@@ -1,3 +1,4 @@
+import { faker } from "@faker-js/faker";
 import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const albumsApi = createApi({
@@ -17,9 +18,18 @@ export const albumsApi = createApi({
                         method: "GET"
                     }
                 }
+            }),
+            addAlbum: builder.mutation({
+                query: (user) => {
+                    return {
+                        url: "/albums",
+                        method: "POST",
+                        body: { userId: user.id, title: faker.commerce.productName() }
+                    }
+                }
             })
         }
     }
 });
 
-export const { useGetAlbumsQuery } = albumsApi;
+export const { useGetAlbumsQuery, useAddAlbumMutation } = albumsApi;
