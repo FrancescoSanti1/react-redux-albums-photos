b93476d71613bdf7b1e9b27892173f830590469c
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-09-03 11:12:00 +0200

    link new slice to the global store

--- src/store/index.js
+++ src/store/index.js
@@ -5,16 +5,21 @@ import { addUser } from "./thunks/addUser";
 import { deleteUser } from "./thunks/deleteUser";
 import { albumsApi } from "./apis/albums";
 import { setupListeners } from "@reduxjs/toolkit/dist/query";
+import { photosApi } from "./apis/photos";
 
 const store = configureStore({
     reducer: {
         users: usersReducer,
-        [albumsApi.reducerPath]: albumsApi.reducer
+        [albumsApi.reducerPath]: albumsApi.reducer,
+        [photosApi.reducerPath]: photosApi.reducer
     },
-    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(albumsApi.middleware)
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware()
+        .concat(albumsApi.middleware)
+        .concat(photosApi.middleware)
 });
 
 setupListeners(store.dispatch);
 
 export { store, fetchUsers, addUser, deleteUser };
 export { useGetAlbumsQuery, useAddAlbumMutation, useDeleteAlbumMutation } from "./apis/albums";
+export { useGetPhotosQuery, useAddPhotoMutation, useDeletePhotoMutation } from "./apis/photos";
