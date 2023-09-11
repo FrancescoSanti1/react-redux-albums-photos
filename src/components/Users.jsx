9ee98ed75b0ae260f42d27841f03ad154b87b943
Francesco Santi <88621180+FrancescoSanti1@users.noreply.github.com>
2023-09-11 23:16:04 +0200

    refactor users components with RTK query hooks

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,30 +1,21 @@
-import { useEffect } from "react";
-import { useSelector } from "react-redux";
-import { addUser, fetchUsers } from "../store";
+import { useAddUserMutation, useGetUsersQuery } from "../store";
 import Skeleton from "./Skeleton";
 import Button from "./Button";
-import useThunk from "../hooks/useThunk";
 import User from "./User";
 
 export default function Users() {
 
-    const [runFetchUsers, loadingUsers, loadingError] = useThunk(fetchUsers);
-    const [runAddUser, creatingUser, creatingError] = useThunk(addUser);
-
-    const { data } = useSelector(state => state.users);
-
-    useEffect(() => {
-        runFetchUsers();
-    }, []);
+    const { data, isFetching, error } = useGetUsersQuery();
+    const [addUser, results] = useAddUserMutation();
 
     const handleUserAdd = () => {
-        runAddUser();
+        addUser();
     };
 
     let content;
-    if (loadingUsers) {
+    if (isFetching) {
         content = <Skeleton times={6} className={"h-10 w-full"} />;
-    } else if (loadingError) {
+    } else if (error) {
         content = <div>Error during data fetching...</div>;
     } else {
         content = data.map(user => <User key={user.id} user={user} />);
@@ -33,7 +24,7 @@ export default function Users() {
     return <div>
         <div className="flex justify-between items-center m-3">
             <h1 className="m-2 text-xl">Users</h1>
-            <Button loading={creatingUser} variation={"primary"} onClick={handleUserAdd}>Add user</Button>
+            <Button loading={results.isLoading} variation={"primary"} onClick={handleUserAdd}>Add user</Button>
         </div>
         {content}
     </div>
